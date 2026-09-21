e05996d018c54a51c7d1cd41861f0d55f9e97063
John Paul <johnpaulcarciller@gmail.com>
2026-09-21 12:04:32 +0800

    sample

---
+++ grade.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+
+    printf("Enter a your score:");
+    scanf("%d", &score);
+
+    printf("Your grade is %d -", score);
+    if (score >= 90){
+        printf("A\n");
+    }
+    else if (score >= 80){
+        printf("B\n");
+    }
+    else if (score >=70){
+        printf("C\n");
+    }
+    else if (score >=60){
+        printf("D\n");
+    }
+    else{
+        printf("F\n");
+    }
+
+    return 0;
+    
+}
